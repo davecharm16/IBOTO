65f18002d3c5b2c7131addf7a68b168f4b7dfb7d
davecharm16 <davecharm16@gmail.com>
2022-03-23 07:20:01 +0800

    first commit

---
+++ app/src/main/java/com/example/iboto/Cards.java
@@ -0,0 +1,27 @@
+package com.example.iboto;
+
+public class Cards {
+    private String imgUrl;
+    private String name;
+
+    public Cards(String imgUrl, String name) {
+        this.imgUrl = imgUrl;
+        this.name = name;
+    }
+
+    public String getImgUrl() {
+        return imgUrl;
+    }
+
+    public void setImgUrl(String imgUrl) {
+        this.imgUrl = imgUrl;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
